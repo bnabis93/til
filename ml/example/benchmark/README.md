193d1816623b6aa386924ab26429fcc70fac0dca
hyeonwoojeong <qhsh9713@gmail.com>
2022-05-12 00:16:36 +0900

    Add benchmark example code

---
+++ ml/example/benchmark/README.md
@@ -0,0 +1,52 @@
+# Pytorch에서 확실하게 model inference speed benchmark를 하는 방법
+## 통제해야하는 변수
+- GPU warmup
+- GPU 동기화
+- CUDA 시간 측정
+
+## 왜 통제해야하는가?
+### GPU warmup
+- GPU는 여러 state를 가질 수 있다.
+    - 예를 들어 오랫동안 사용하지 않고있으면 저전력 상태로 바뀐다.
+    - A modern GPU device can exist in one of several different power states. When the GPU is not being used for any purpose and persistence mode (i.e., which keeps the GPU on) is not enabled, the GPU will automatically reduce its power state to a very low level, sometimes even a complete shutdown - [ref](https://towardsdatascience.com/the-correct-way-to-measure-inference-time-of-deep-neural-networks-304a54e5187f)
+
+### GPU 동기화
+```python
+torch.cuda.synchronize()
+
+# 그러니까 이렇게 측정하지 말라 이거지.
+s = time.time()
+ _ = model(dummy_input)
+curr_time = (time.time()-s )*1000
+```
+- Waits for all kernels in all streams on a CUDA device to complete. - [ref](https://pytorch.org/docs/stable/generated/torch.cuda.synchronize.html)
+- PyTorch automatically performs necessary synchronization when copying data between CPU and GPU or between two GPUs. - [ref](https://leimao.github.io/blog/PyTorch-Benchmark/)
+- 즉, torch에서 (정확히는 CUDA에서) GPU-CPU 연산(데이터를 복사하는 등의) 혹은 두개의 GPU간의 연산은 비동기적으로 일어나므로, benchmark시 동기화를 해주어야 한다.
+- 특히 Time 특정 할때 문제가 된다. time은 cpu에서, model 연산은 gpu에서 비동기적으로 진행되기 때문이다.
+    - GPU의 연산이 비동기로 일어난다면, gpu 연산이 끝나기전에 시간이 측정 될 수도 있다.
+
+### 시간 측정
+- 일반적으로 cpu에서 일어나는 일은 python의 `time` 혹은 `timeit`을 이용하여 측정한다.
+- CUDA / CUDA Evnet의 경우 Pytorch의 torch.cuda.Event (CUDA Evnet Wrapper)로 측정가능하다.
+
+
+## Prerequisites
+- You should use GPU machine for this example.
+- Maybe you should modify Makefile's cudatoolkit to the version that suits your GPU.
+```bash
+make env
+conda activate benchmark
+make setup
+```
+
+## How to run
+- 실험에서 if문의 context swiching 비용은 무시한다.
+```bash
+
+```
+
+
+## Reference
+- https://towardsdatascience.com/the-correct-way-to-measure-inference-time-of-deep-neural-networks-304a54e5187f
+- https://leimao.github.io/blog/PyTorch-Benchmark/
+- https://pytorch.org/docs/stable/generated/torch.cuda.synchronize.html
---
+++ ml/example/benchmark/benchmark.py
@@ -0,0 +1,85 @@
+import argparse
+import time
+from typing import Tuple
+
+import as
+import numpy as np
+import torch
+import torchvision.models as models
+
+parser = argparse.ArgumentParser(description="PyTorch benchmark example")
+parser.add_argument(
+    "--warmup",
+    type=bool,
+    default=True,
+    help="If you set this value to true, GPU warmup process is started.",
+)
+parser.add_argument(
+    "--cuda_sync",
+    type=bool,
+    default=True,
+    help="Cuda sync on(True) / off(False).",
+)
+parser.add_argument(
+    "--cuda_time",
+    type=bool,
+    default=True,
+    help="CUDA Timer on(True) / off(False).",
+)
+args = parser.parse_args()
+
+def benchmark(
+    model: torch.nn.Module,
+    input_shape: Tuple(int, int, int, int) = (1, 1, 224, 224),
+    nwarmup : int =50,
+    nruns : int =1000,
+    device : str,
+):
+    """Benchmark module."""
+    input_data = torch.randn(input_shape)
+    input_data = input_data.to(device)
+    with torch.no_grad():
+        if args.warmup:
+            for _ in range(nwarmup):
+                features = model(input_data)
+    
+    if args.cuda_sync:
+        torch.cuda.synchronize()
+    timings = []
+    with torch.no_grad():
+        for i in range(1, nruns + 1):
+            if args.cuda_time:
+                start_time, end_time = torch.cuda.Event(enable_timing=True), torch.cuda.Event(enable_timing=True) 
+                start_time.record()
+            else:
+                start_time = time.time()
+            features = model(input_data)
+            if args.cuda_sync:
+                torch.cuda.synchronize()
+            if args.cuda_time:
+                end_time.record()
+                curr_time = start_time.elapsed_time(end_time)/1000
+            else:
+                end_time = time.time()
+                curr_time = end_time - start_time 
+                
+            timings.append(curr_time)
+
+    print("Average time: %.2f ms" % (np.mean(timings) * 1000))
+
+
+def main():
+    """Main fucntion.
+    This function will be set benchmark preparation thing.
+    """
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    resnet50 = models.resnet50(pretrained=True)
+    resnet50.eval().to(device)
+    print(f"warmup {args.warmup} cuda sync {args.cuda_sync} cuda time {args.cuda_time}")
+    benchmark(model= resnet50, device= device)
+
+    return
+
+
+if __name__ == "__main__":
+    main()
