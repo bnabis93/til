c9efd7b238fcb740be2324ba44ecc4c7c6f015bd
hyeonwoojeong <qhsh9713@gmail.com>
2022-07-25 00:20:32 +0900

    Add basic create and list api

--- java/spring-boot/00-basic-crud/src/main/java/bono/basiccrud/controller/MemberController.java
+++ java/spring-boot/00-basic-crud/src/main/java/bono/basiccrud/controller/MemberController.java
@@ -5,13 +5,15 @@ import bono.basiccrud.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 // Spring bean이 관리된다.
 // Spring 프로젝트 빌드 후 실행 -> spring 컨테이너가 뜨는데, 특정 annotation 들은 spring 이 관리함. 이를 보고 bean이 관리된다고 한다.
+
+//@RestController
 @Controller
 public class MemberController {
 
@@ -42,5 +44,26 @@ public class MemberController {
         List<Member> members = memberService.findMembers();
         model.addAttribute("members" , members);
         return "members/memberList";
+    }
+
+    @ResponseBody
+    @PostMapping("/members/create")
+    public String createMember(@RequestBody Member mem){
+        Member member = new Member();
+        System.out.println("mem name : " +mem.getName() );
+        member.setName(mem.getName());
+        memberService.join(member);
+        return "Member created";
+    }
+
+    @ResponseBody
+    @GetMapping("/members/list")
+    public String getMembers(){
+        List<Member> members = memberService.findMembers();
+        List<String> memberLists = new ArrayList<>();
+        for (Member member : members){
+            memberLists.add(member.getName());
+        }
+        return memberLists.toString();
     }
 }
