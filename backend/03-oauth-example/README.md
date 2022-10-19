78d44c9df248b69a25612c63d30bea38487f9375
bono <qhsh9713@gmail.com>
2022-10-19 11:41:47 +0900

    Add oauth 2.0 config setting

--- backend/03-oauth-example/README.md
+++ backend/03-oauth-example/README.md
@@ -24,3 +24,5 @@ Authorized redirect URIs: http://127.0.0.1:7000/auth. We are going to create thi
 ```
 
 ## Create OAuth client
+- We need the `client_id` and `the client_secret`.
+- Use the environment variables `GOOGLE_CLIENT_ID` and `GOOGLE_CLIENT_SECRET`
--- backend/03-oauth-example/src/main.py
+++ backend/03-oauth-example/src/main.py
@@ -6,14 +6,38 @@ from https://fastapi.tiangolo.com/tutorial/body/
 """
 
 
+import os
+
+from authlib.integrations.starlette_client import OAuth
 from fastapi import FastAPI
 from pydantic import BaseModel
+from starlette.config import Config
 
 app = FastAPI()
 
+###################
+# Oauth setting
+###################
+GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID') or None
+GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET') or None
+if GOOGLE_CLIENT_ID is None or GOOGLE_CLIENT_SECRET is None:
+    raise BaseException('Missing env variables')
+
+auth_config =  {'GOOGLE_CLIENT_ID': GOOGLE_CLIENT_ID, 'GOOGLE_CLIENT_SECRET': GOOGLE_CLIENT_SECRET}
+starlette_config = Config(environ=auth_config)
+oauth = OAuth(starlette_config)
+oauth.register(
+    name='google',
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+    client_kwargs={'scope': 'openid email profile'},
+)
+
+
+
+###################
+# APIs
+###################
 @app.get("/healthcheck")
 async def healthcheck() -> bool:
     """Server healthcheck."""
     return True
-
-
