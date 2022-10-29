db42d7defa4321435dd5b5b8f1aa85e21e458aec
bono <qhsh9713@gmail.com>
2022-10-30 02:35:15 +0900

    Add google OAuth example

--- backend/fastapi/03-oauth-example/src/main.py
+++ backend/fastapi/03-oauth-example/src/main.py
@@ -6,56 +6,36 @@
 
 
 import json
-import os
-from typing import Any, Dict
 
+import requests
 from authlib.integrations.starlette_client import OAuth, OAuthError
 from fastapi import FastAPI, Request
-from starlette.config import Config
 from starlette.middleware.sessions import SessionMiddleware
 from starlette.responses import HTMLResponse, RedirectResponse
 
-###################
-# Oauth setting
-###################
-GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID') or None
-GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET') or None
-SECRET_KEY = os.environ.get('SECRET_KEY') or None
-if GOOGLE_CLIENT_ID is None or GOOGLE_CLIENT_SECRET is None:
-    raise BaseException('Missing env variables')
-if SECRET_KEY is None:
-    raise BaseException("Missing SECRET_KEY")
-
-auth_config = {'GOOGLE_CLIENT_ID': GOOGLE_CLIENT_ID, 'GOOGLE_CLIENT_SECRET': GOOGLE_CLIENT_SECRET}
-starlette_config = Config(environ=auth_config)
-oauth = OAuth(starlette_config)
-oauth.register(
-    name='google',
-    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
-    client_kwargs={'scope': 'openid email profile'},
-)
-
+from src.config import GOOGLE_CONF_URL, configs
 
 # Create app
 app = FastAPI()
-app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY)
-
-
+app.add_middleware(SessionMiddleware, secret_key="SECRET_KEY")
+oauth = OAuth(configs)
+oauth.register(
+    name="google",
+    server_metadata_url=GOOGLE_CONF_URL,
+    client_kwargs={"scope": "openid email profile"},
+)
 ###################
 # APIs
 ###################
 
 
-@app.get('/')
+@app.get("/")
 async def homepage(request: Request):
     """Root."""
-    user = request.session.get('user')
+    user = request.session.get("user")
     if user:
         data = json.dumps(user)
-        html = (
-            f'<pre>{data}</pre>'
-            '<a href="/logout">logout</a>'
-        )
+        html = f"<pre>{data}</pre>" '<a href="/logout">logout</a>'
         return HTMLResponse(html)
     return HTMLResponse('<a href="/login">login</a>')
 
@@ -67,9 +47,12 @@ async def healthcheck() -> bool:
 
 
 @app.get("/info")
-async def show_info() -> dict[str, str]:
+async def info() -> dict[str, str]:
     """Show auth config info."""
-    return auth_config
+    return {
+        "GOOGLE_CLIENT_ID": configs("GOOGLE_CLIENT_ID"),
+        "GOOGLE_CLIENT_SECRET": configs("GOOGLE_CLIENT_SECRET"),
+    }
 
 
 ###################
@@ -78,20 +61,33 @@ async def show_info() -> dict[str, str]:
 
 
 @app.get("/login")
-async def login(request : Request):
-    """Oauth Login."""
-    redirect_uri = request.url_for('auth')
+async def login(request: Request):
+    """Login api."""
+
+    # Redirect to /auth
+    redirect_uri = request.url_for("auth")
+    print("Redirection to /auth : ", redirect_uri)
+
     return await oauth.google.authorize_redirect(request, redirect_uri)
 
 
-@app.get('/auth')
-async def auth(request: Request) -> Dict[Any, Any]:
-    """Auth api."""
+@app.get("/auth")
+async def auth(request: Request):
+    """Auth."""
+
     try:
         token = await oauth.google.authorize_access_token(request)
     except OAuthError as error:
-        return HTMLResponse(f'<h1>{error.error}</h1>')
+        return HTMLResponse(f"<h1>{error.error}</h1>")
     user = token.get("userinfo")
+    print("user : ", user)
     if user:
-        request.session['user'] = dict(user)
-    return RedirectResponse(url='/')
+        request.session["user"] = dict(user)
+    return RedirectResponse(url="/")
+
+
+@app.get("/logout")
+async def logout(request: Request):
+    """logout."""
+    request.session.pop("user", None)
+    return RedirectResponse(url="/")
